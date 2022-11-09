9c120b6b418953daef39fb02506fdc5e2438e625
LowstoneGithub <antony.berbertdecastrohuesler@unibas.ch>
2022-11-09 10:42:33 +0100

    Add Project files (R, Papers, Notes)

---
+++ R/consumerdata_SFO.R
@@ -0,0 +1,7 @@
+library(readxl)
+getwd()
+setwd("C:/Users/oOAnt/Documents/UniBas/HS2022/Masterprojekt/Master-Project")
+consumerdf <- read_excel("Data/consumerflying_clean.xlsx")
+View(consumerdf)
+
+
